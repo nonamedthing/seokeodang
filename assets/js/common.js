df3d217217ffa255d0ae44d838d444acb8f03b9f
nonamedthing <s2ul.won@gmail.com>
2024-07-16 21:54:35 +0900

    Add files via upload

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,264 +1,132 @@
 const common =
     {
-        goods: [
-            {
-                idx: 1,
-                name: "스티커 (25종 개별)",
-                img: "./assets/images/goods/image10.png",
-                price: 2000,
-            },
-            {
-                idx: 2,
-                name: "금속배지",
-                options: [
-                    {
-                        type: "type",
-                        option: ["대감", "무궁화"],
-                    },
-                ],
-                img: "./assets/images/goods/image11.png",
-                price: 12000,
-            },
-            {
-                idx: 3,
-                name: "캔배지 (3종 1세트)",
-                img: "./assets/images/goods/image12.png",
-                price: 8000,
-            },
-            {
-                idx: 4,
-                name: "티셔츠",
-                options: [
-                    {
-                        type: "color",
-                        option: ["white", "black"],
-                    },
-                    {
-                        type: "size",
-                        option: ["m", "l", "xl"],
-                    },
-                ],
-                img: "./assets/images/goods/image07.png",
-                price: 35000,
-            },
-            {
-                idx: 5,
-                name: "공무원증 세트",
-                img: "./assets/images/goods/image08.png",
-                price: 15000,
-            },
-            {
-                idx: 6,
-                name: "에코백",
-                img: "./assets/images/goods/image09.png",
-                price: 18000,
-            },
-            {
-                idx: 7,
-                name: "디오라마",
-                options: [
-                    {
-                        type: "type",
-                        option: ["석어당", "역적"],
-                    },
-                ],
-                img: "./assets/images/goods/image05.png",
-                price: 29000,
-            },
-            {
-                idx: 8,
-                name: "아크릴 스탠드",
-                options: [
-                    {
-                        type: "type",
-                        option: ["1. 출근", "2. 흠", "3. 무관", "4. 학창의", "5. 구라파", "6. 종묘", "7. 뛰뛰"],
-                    },
-                ],
-                img: "./assets/images/goods/image06.png",
-                price: 9000,
-            },
-            {
-                idx: 9,
-                name: "죽창 스마트톡",
-                img: "./assets/images/goods/image01.png",
-                price: 15000,
-            },
-            {
-                idx: 10,
-                name: "합죽선",
-                img: "./assets/images/goods/image02.png",
-                price: 15000,
-            },
-            {
-                idx: 11,
-                name: "공명첩 손수건",
-                img: "./assets/images/goods/image03.png",
-                price: 15000,
-            },
-            {
-                idx: 12,
-                name: "페이스 쿠션",
-                img: "./assets/images/goods/image04.png",
-                price: 36000,
-            }
-        ],
-        init: function () {
+        serviceKey : 'wi0HturBsF0NNnifZD1S50ta2vXAgK7AXz8a/Ssf0FJWyw/r6tsJkotxBQ7XMg0t7EwhpyOB8UkNiBoUshvpMg==',
+        wetherUrl : 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst',
+        riseUrl : 'http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getAreaRiseSetInfo',
+        nx : '60',
+        ny : '127',
+        hour : 0,
+        sunrise : 0,
+        sunset : 0,
+        interval : null,
+        init : function () {
+            this.getRise();
+            this.events();
         },
+        events : function () {
+            setTimeout(function () {
+                common.setNight();
+            }, 500);
+            this.reset();
 
-        convertGoodHtml: function (item) {
-            let html = ``;
-            html += `<div class="col item-card" data-idx="${item.idx}">`;
-            html += `<img src="${item.img}">`;
-            html += `<div class="name">${item.name}</div>`;
-            html += `<div class="price" data-price="${item.price}">${item.price.toLocaleString()}원</div>`;
-            // option 추가
-            if (item.options !== undefined) {
-                html += `<div class="options">`;
-                html += `<div class="option input-group option${item.idx}">`;
-                $(item.options).each(function (index) {
-                    let option = item.options[index];
-                    html += `<select class="custom-select" name="${item.idx}[${option.type}][]">`;
-                    $(option.option).each(function (index) {
-                        let optionItem = option.option[index];
-                        html += `<option value="${optionItem}">${optionItem}</option>`;
-                    });
-                    html += `</select>`;
-                });
-                html += `<select class="custom-select qty qty-option" name="option[${item.idx}]['qty'][]">`;
-                html += common.bindQtyOptions();
-                html += `</select>`;
-                html += `<button type="button" class="add btn btn-sm btn-outline-secondary" data-idx="${item.idx}">추가</button>`;
-                html += `</div>`;
+            this.interval = setInterval(function () {
+                // 날짜가 바뀌면 일출/일몰 계산
+
+                if (hour != common.hour) {
+                    common.reset();
+                }
+            }, 360000);
+        },
+        reset : function () {
+            this.bindCurrentHour();
+            $('.container').append('<div>'+this.getCurrentDate()+ ' '+this.getCurrentHour()+'기준 </div>');
+            this.getWether();       
+        },
+        getCurrentDate : function () {
+            let date = new Date();
+            let month = date.getMonth()+1;
+            if (month < 10) {
+                month = '0'+month;
+            }
+            return date.getFullYear() + '' + month +''+ date.getDate();
+        },
+        bindCurrentHour : function () {
+            let date = new Date();
+            this.hour = date.getHours();
+        },
+        getCurrentHour : function () {
+            return this.hour +'00';
+        },
+        getRise : function () {
+            $.ajax({    
+                type : 'get',
+                url : this.riseUrl,
+                async : true,
+                data : {
+                    serviceKey : this.serviceKey,
+                    locdate : this.getCurrentDate(),
+                    location : '서울',
+                },
+                success : function (result) {
+                    let obj = $.xml2json(result);
+                    let items = obj.body.items.item;
+                    common.sunrise = items.sunrise;
+                    common.sunset = items.sunset;
+                    $('.container').append('<div>일출 : '+common.sunrise+ '</div>');
+                    $('.container').append('<div>일몰 : '+common.sunset+ '</div>');
+                },
+                error : function (error) {
+                    console.log(error);
+                }
+            });
+        },
+        getWether : function () {
+            $.ajax({    
+                type : 'get',
+                url : this.wetherUrl,
+                async : true,
+                data : {
+                    serviceKey : this.serviceKey,
+                    pageNo : 1,
+                    dataType : 'JSON',
+                    base_date : this.getCurrentDate(),
+                    base_time : this.getCurrentHour(),
+                    nx : this.nx,
+                    ny : this.ny,
+                },
+                success : function (result) {
+                    let items = result.response.body.items.item;
+                    common.bindWetherResults(items);
+                },
+                error : function (error) {
+                    console.log(error);
+                }
+            });
+        },
+        bindWetherResults : function (items) {
+            $(items).each(function (index) {
+                let item = items[index];
+                switch (item.category) {
+                // 강수형태
+                // case 'PTY' :
+                //     common.setRain(item.obsrValue > 0);
+                //     break;
+                // 1시간 강수량
+                case 'RN1' :
+                    common.setRain(item.obsrValue > 0);
+                    $('.container').append('<div>비옴</div>');
+                    break;
+                }
+            });
+        },
+        setRain : function (bool) {
+            if (bool) {
+                $('.container').addClass('rain');
             } else {
-                html += `<select class="custom-select qty" name="option[${item.idx}]['qty']">`;
-                html += common.bindQtyOptions(item.idx == 1 ? 101 : 0);
-                html += `</select>`;
+                $('.container').removeClass('rain');
             }
-            html += `</div>`;
-            html += `</div>`;
-            return html;
         },
-        bindQtyOptions: function (length = 0) {
-            let html = ``;
-            for (int in Array.from(Array(length == 0 ? 11 : length).keys())) {
-                html += `<option value="${int}">${int}</option>`;
+        setNight : function () {            
+            let date = new Date();
+            hour = date.getHours();
+            minute = date.getMinutes();
+            let time = hour + '' + minute;
+
+            let bool = this.sunrise < time || this.sunset > time
+            if (bool) {
+                $('.container').addClass('night');
+            } else {
+                $('.container').removeClass('night');
             }
-
-            return html;
-        },
-        setGoods: function (element) {
-            let html = ``;
-            $(this.goods).each(function (index) {
-                let item = common.goods[index];
-                html += common.convertGoodHtml(item);
-            });
-
-            element.html(html);
-        },
-        cloneOption: function (element) {
-            let idx = element.attr('data-idx');
-            let clone = $(`.option${idx}`).last().clone();
-            clone.children('.add').remove();
-            clone.append(`<button type="button" class="remove btn btn-sm btn-outline-danger" data-idx="${idx}">X</button>`);
-            let originSelects = element.parent().find('select');
-            clone.find('select').each(function (index) {
-                $(this).val(originSelects.eq(index).val());
-            });
-
-            element.parents('.options').prepend(clone);
-            common.calcAmount();
-        },
-        calcAmount : function () {
-            let amount = 0;
-
-            $('select.qty').each(function (index) {
-                let select = $(this);
-                let parent = select.parents('.item-card');
-                let qty = parseInt(select.val());
-
-                if (qty == 0) {
-                    return;
-                }
-
-                if (select.hasClass('qty-option')) {
-                    let optionParent = select.parents('.option');
-                    if (optionParent.has('.add').length > 0) {
-                        return;
-                    }
-                }
-
-                let price = parent.find('.price').attr('data-price');
-                let totalPrice = qty * price;
-
-                amount += totalPrice;
-            });
-            $('#amountInfo .amount').html(amount.toLocaleString() + '원');
-            $('#amountInfo .gift').html((parseInt(amount / 20000)).toLocaleString() + '개');
-        },
-        saveImage: function () {
-            let html = ``;
-            let amount = 0;
-
-            $('select.qty').each(function (index) {
-                let select = $(this);
-                let parent = select.parents('.item-card');
-                let qty = parseInt(select.val());
-
-                if (qty == 0) {
-                    return;
-                }
-
-                let name = parent.find('.name').text();
-                let img = parent.find('img').attr('src');
-
-                if (select.hasClass('qty-option')) {
-                    let optionParent = select.parents('.option');
-                    if (optionParent.has('.add').length > 0) {
-                        return;
-                    }
-
-                    let optionText = ``;
-                    $(optionParent).find('select').not('.qty').each(function () {
-                        let value = $(this).val();
-                        if (optionText != '') {
-                            optionText += '/';
-                        }
-
-                        optionText += value;
-                    });
-
-                    if (optionText != '') {
-                        name += ` (${optionText})`;
-                    }
-                }
-
-                let price = parent.find('.price').attr('data-price');
-                let totalPrice = qty * price;
-
-                amount += totalPrice;
-
-                html += `<div class="item clearfix">`;
-                html += `<span class="left"><img src="${img}"></span>`;
-                html += `<span class="left">${name}</span>`;
-                html += `<span class="right">`;
-                html += `<span class="price">${qty}*${price.toLocaleString()}</span>`;
-                html += `<span class="total">${(totalPrice).toLocaleString()}원</span>`;
-                html += `</span>`;
-                html += `</div>`;
-            });
-            $('#amountInfo .amount').html(amount.toLocaleString() + '원');
-            $('#amountInfo .gift').html((parseInt(amount / 20000)).toLocaleString() + '개');
-
-            $('#wrapItems').hide();
-            $('#itemList').html(html).show();
-            $('.signature').show();
-
-            html2canvas(document.querySelector('#receipt')).then(function (canvas) {\
-                $('#preview').append(`<img src="` + canvas.toDataURL("image/png", 1.0) + `">`).show();
-                $('.image-btn a').attr('href', canvas.toDataURL("image/png", 1.0));
-                $('#receipt').hide();
-                $('.preview-btn').hide();
-                $('.image-btn').show();
-            });
         }
     }
